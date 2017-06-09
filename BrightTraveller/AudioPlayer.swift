89210492604797302c21cd932c443bae3bd409d0
Eliasz Sawicki <eliasz.sawicki@gmail.com>
2017-06-09 14:39:40 +0200

    Comment out audio player

--- BrightTraveller/AudioPlayer.swift
+++ BrightTraveller/AudioPlayer.swift
@@ -12,48 +12,47 @@ import AVFoundation
 class AudioPlayer {
     
     static let sharedInstance = AudioPlayer()
-    let queuePlayer = AVQueuePlayer()
+//    let queuePlayer = AVQueuePlayer()
     
     
     
     func playWelcomeAudio() {
         print("*AUDIO PLAYER* playing welcome")
-        
-        let urlPath = Bundle.main.path(forResource: "bt_welcome", ofType: "m4a")
-        let fileURL = URL(fileURLWithPath:urlPath!)
-        let playerItem = AVPlayerItem(url: fileURL)
-        queuePlayer.insert(playerItem, after: nil)
-        queuePlayer.play()
+//        let urlPath = Bundle.main.path(forResource: "bt_welcome", ofType: "m4a")
+//        let fileURL = URL(fileURLWithPath:urlPath!)
+//        let playerItem = AVPlayerItem(url: fileURL)
+//        queuePlayer.insert(playerItem, after: nil)
+//        queuePlayer.play()
     }
     
     func playRideDecisionAudio() {
         print("*AUDIO PLAYER* playing ride decision")
-        let urlPath = Bundle.main.path(forResource: "bt_initializer", ofType: "m4a")
-        let fileURL = URL(fileURLWithPath:urlPath!)
-        let playerItem = AVPlayerItem(url: fileURL)
-        queuePlayer.insert(playerItem, after: nil)
-        queuePlayer.play()
+//        let urlPath = Bundle.main.path(forResource: "bt_initializer", ofType: "m4a")
+//        let fileURL = URL(fileURLWithPath:urlPath!)
+//        let playerItem = AVPlayerItem(url: fileURL)
+//        queuePlayer.insert(playerItem, after: nil)
+//        queuePlayer.play()
     }
     
     func playOnAcceptAudio() {
         print("*AUDIO PLAYER* playing ride accept audio")
         
-        let urlPath = Bundle.main.path(forResource: "bt_accept", ofType: "m4a")
-        let fileURL = URL(fileURLWithPath:urlPath!)
-        let playerItem = AVPlayerItem(url: fileURL)
-        queuePlayer.insert(playerItem, after: nil)
-        queuePlayer.play()
+//        let urlPath = Bundle.main.path(forResource: "bt_accept", ofType: "m4a")
+//        let fileURL = URL(fileURLWithPath:urlPath!)
+//        let playerItem = AVPlayerItem(url: fileURL)
+//        queuePlayer.insert(playerItem, after: nil)
+//        queuePlayer.play()
         
     }
     
     func playOnDiscardAudio() {
         print("*AUDIO PLAYER* laying ride discard audio")
         
-        let urlPath = Bundle.main.path(forResource: "bt_welcome", ofType: "m4a")
-        let fileURL = URL(fileURLWithPath:urlPath!)
-        let playerItem = AVPlayerItem(url: fileURL)
-        queuePlayer.insert(playerItem, after: nil)
-        queuePlayer.play()
+//        let urlPath = Bundle.main.path(forResource: "bt_welcome", ofType: "m4a")
+//        let fileURL = URL(fileURLWithPath:urlPath!)
+//        let playerItem = AVPlayerItem(url: fileURL)
+//        queuePlayer.insert(playerItem, after: nil)
+//        queuePlayer.play()
     }
     
 }
